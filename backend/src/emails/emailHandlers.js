a5f088eaf6f3560c7488523f75266620d747ae4e
rohan <bhawarerohan24@gmail.com>
2025-12-09 23:14:38 +0530

    welcome email

--- backend/src/emails/emailHandlers.js
+++ backend/src/emails/emailHandlers.js
@@ -1,10 +1,33 @@
 import { resendClient, sender } from "../lib/resend.js";
 import { createWelcomeEmailTemplate } from "../emails/emailTemplates.js";
 
+// export const sendWelcomeEmail = async (email, name, clientURL) => {       after domane name
+//   const { data, error } = await resendClient.emails.send({
+//     from: `${sender.name} <${sender.email}>`,
+//     to: email,
+//     subject: "Welcome to Chatify!",
+//     html: createWelcomeEmailTemplate(name, clientURL),
+//   });
+
+//   if (error) {
+//     console.error("Error sending welcome email:", error);
+//     throw new Error("Failed to send welcome email");
+//   }
+
+//   console.log("Welcome Email sent successfully", data);
+// };
+
+
+
+
+
 export const sendWelcomeEmail = async (email, name, clientURL) => {
+  const fromField = `${sender.name} <${sender.email}>`;
+  console.log("FROM FIELD =>", fromField);
+
   const { data, error } = await resendClient.emails.send({
-    from: `${sender.name} <${sender.email}>`,
-    to: email,
+    from: fromField,
+    to: "rohanbhaware76@gmail.com",
     subject: "Welcome to Chatify!",
     html: createWelcomeEmailTemplate(name, clientURL),
   });
